d7c353fa7ea01bd3756b8f25a1148fdb90825191
Molly Trepanier <mtrepanier123@gmail.com>
2021-04-12 19:09:35 -0700

    added classNames lib to component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,9 +1,14 @@
 import React from "react";
-import classnames from "classnames"
+import classNames from "classnames"
 import "components/styling/DayListItem.scss"
 
 
 const DayListItem = (props) => {
+  const dayClass = classNames({
+    "day--list__item": true,
+    "day--list__item--selected": props.selected,
+    "day--list__item--full": props.spots === 0
+  })
   return (
     <li onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
