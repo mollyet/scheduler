35e81ce1c991cb434f91455c5e650fb3f50547a5
Molly Trepanier <mtrepanier123@gmail.com>
2021-04-12 19:01:40 -0700

    created styling folder to sequester all of my scss files away from the components

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,6 @@
 import React from "react";
 import classnames from "classnames"
-import "components/Button.scss";
+import "components/styling/Button.scss";
 
 export default function Button(props) {
 
