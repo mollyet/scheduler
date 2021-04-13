f9ad357748a97acf487e1eb9e10d9b7f9b91515c
Molly Trepanier <mtrepanier123@gmail.com>
2021-04-12 19:18:51 -0700

    created function to render how many spots are remaining

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,6 +1,6 @@
 import React from "react";
-import classNames from "classnames"
-import "components/styling/DayListItem.scss"
+import classNames from "classnames";
+import "components/styling/DayListItem.scss";
 
 
 const DayListItem = (props) => {
@@ -8,13 +8,27 @@ const DayListItem = (props) => {
     "day--list__item": true,
     "day--list__item--selected": props.selected,
     "day--list__item--full": props.spots === 0
-  })
+  });
+
+  const formatSpots = (spots) => {
+    let spot = "";
+    if (spots === 0) {
+      spot = "Sorry, no spots remaining!";
+    }
+    if (spots === 1) {
+      spot = "One spot remaining!";
+    }
+    if (spots >= 2) {
+      spot = `${spots} spots remaning!`;
+    }
+    return spot; 
+  };
   return (
     <li onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 classname="text--light">{props.spots}</h3>
+      <h3 classname="text--light">{formatSpots(props.spots)}</h3>
     </li>
-  )
-}
+  );
+};
 
-export default DayListItem; 
+export default DayListItem;
