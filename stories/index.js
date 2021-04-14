8ec0d281917faf16848962b416ebdc5b033df48a
Molly Trepanier <mtrepanier123@gmail.com>
2021-04-13 21:24:50 -0700

    added first stories for appoitnment component

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,7 @@ import DayListItem from "components/DayListItem"
 import DayList from "components/DayList"
 import InterviewerListItem from "components/InterviewerListItem"
 import InterviewerList from "components/InterviewerList"
+import Appointment from "components/Appointment/index"
 
 //button stories 
 
@@ -136,4 +137,12 @@ storiesOf("Button", module)
             setInterviewer={action("setInterviewer")}
           />
         ));
-      
+
+        // Appointment Stories
+
+        storiesOf("Appointment", module)
+        .addParameters({
+          backgrounds: [{ name: "white", value: "#ffffff", default: true}]
+        })
+        .add("Appointment", () => <Appointment />)
+        .add("Appointment with time", () => <Appointment time="12pm" />);
