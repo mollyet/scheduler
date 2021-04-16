4fc03f0ce58c4100c0485ceeb607d1acaca99594
Molly Trepanier <mtrepanier123@gmail.com>
2021-04-15 21:13:49 -0700

    create useVisualMode hook && tests for it

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,20 +1,40 @@
 import React from "react";
 //components
-import Header from "components/Appointment/Header"
-import Show from "components/Appointment/Show"
-import Empty from "components/Appointment/Empty"
-//stylings
-import "./styles.scss"
+
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
+
+//hooks
+
+import useVisualMode from "hooks/useVisualMode";
+
+//styling
+
+import "./styles.scss";
+
+// mode constants
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
 
 const Appointment = (props) => {
-  return(
-    <article className="appointment">
-      <Header 
-      time={props.time}
-      />
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
-    </article>
-  )
-}
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
 
-export default Appointment; 
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === SHOW && (
+         <Show
+         student={props.interview.student}
+         interviewer={props.interview.interviewer}
+       />
+      )}
+    </article>
+  );
+};
+
+export default Appointment;
---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+const useVisualMode = (x) => {
+  const [mode, setMode] = useState(x);
+  const [history, setHistory] = useState([x]);
+
+  const transition = (mode, replace = false) => {
+    if (!replace) {
+      setMode(mode);
+      setHistory([...history, mode]);
+    }
+    setMode(mode);
+  };
+
+  const back = (mode) => {
+    if (history.length === 1) {
+      setMode(x);
+    } else {
+      setMode(history[history.length - 2]);
+      setHistory([history.slice(0, -1)]);
+    }
+  };
+
+  return { mode, transition, back };
+};
+
+export default useVisualMode;
